c60676a7c25206d2122dcbead2dd8e175507eede
Nachovn12 <ig.valeria@duocuc.cl>
2026-09-21 19:22:25 -0300

    fix: use S3 index for MSAL redirects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,8 @@ export const environment = {
   frontendClientId: 'b577f07b-edd6-4c6c-8306-93aba3c6feba',
   authority:
     'https://login.microsoftonline.com/f191bdc8-7926-4722-9ec0-cba663701940',
-  redirectUri: typeof window !== 'undefined' ? window.location.origin + '/' : '/',
-  postLogoutRedirectUri: typeof window !== 'undefined' ? window.location.origin + '/' : '/',
+  redirectUri: typeof window !== 'undefined' ? window.location.origin + '/index.html' : '/index.html',
+  postLogoutRedirectUri: typeof window !== 'undefined' ? window.location.origin + '/index.html' : '/index.html',
   apiScope: 'api://a1a87bcd-fed7-4d65-83c3-338389b80093/access_as_user',
   apiGatewayUrl: 'https://ntnnrohhj8.execute-api.us-east-1.amazonaws.com',
   expectedIssuer: 'https://login.microsoftonline.com/f191bdc8-7926-4722-9ec0-cba663701940/v2.0',
